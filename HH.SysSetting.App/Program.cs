104a8c54d5e914cbacf60e92ace8b2844d58df28
Zero <2287991080@qq.com>
2020-05-21 16:30:31 +0800

    增加数据库验证

--- HH.SysSetting.App/Program.cs
+++ HH.SysSetting.App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,6 +10,7 @@ namespace HH.SysSetting.App
 {
     static class Program
     {
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -16,6 +19,19 @@ namespace HH.SysSetting.App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            var connectionStr = ConfigurationManager.ConnectionStrings["DefaultConnection"];
+            if (connectionStr==null)
+            {
+                MessageBox.Show("未配置数据库，请联系管理员！");
+                return;
+            }
+            //SqlConnection sqlConnection = new SqlConnection(connectionStr.ToString());
+            //sqlConnection.Open();
+            //if (sqlConnection.State!=System.Data.ConnectionState.Open)
+            //{
+            //    MessageBox.Show("连接数据库失败，请开发人员检查数据库连接字符串能否正确！");
+            //    return;
+            //}
             Application.Run(new Form1());
         }
     }
